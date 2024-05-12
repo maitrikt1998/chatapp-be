df153c5948fec6020135095a0d5f74dd72ddc0bd
Maitrik <Maitrikt1998@gmail.com>
2024-05-12 19:56:30 +0530

    socket changes

--- socket.cjs
+++ socket.cjs
@@ -1,31 +1,26 @@
-const express = require('express'); 
-const app = express(); 
-const http = require('http'); 
-const { Server } = require('socket.io'); 
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
 
-const server = http.createServer(app); 
-const io = new Server(server, {   
-    cors: { origin: "*" } 
-});  
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" }
+});
 
-app.get('/', (req, res) => {     
-    res.send('<h1>Hello world</h1>');   
-});     
-
-io.on('connection', (socket) => {     
+io.on('connection', (socket) => {
     console.log('A user connected');
     
     socket.on('message', (message) => {
-        console.log('Received message:', message);
-        // You can broadcast this message to other clients if needed
-        // io.emit('message', message);
+        console.log("New Message"+message);
+        io.emit('message', message);
     });
 
-    socket.on('disconnect', () => {             
-        console.log('User disconnected');      
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
     });
-});  
+});
 
-server.listen(3005, () => {   
-    console.log('Socket server is running on port 3005'); 
+server.listen(3005, () => {
+    console.log('Socket server is running on port 3005');
 });
